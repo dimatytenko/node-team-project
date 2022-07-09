c9a866a27712aaaa91e5de74a791caab9de6716b
andrew <s.s.student@khai.edu>
2022-07-09 14:04:20 +0300

    refactor public calc and add new helper

--- controllers/public/calculator.js
+++ controllers/public/calculator.js
@@ -1,37 +1,19 @@
-const { Product } = require('../../models');
 const { calcDailyRate } = require('../../helpers/formulas');
-const calculator = async (req, res, next) => {
-  try {
-    const { blood, height, age, weight_current, weight_desired } = req.body;
-    const dailyRate = calcDailyRate(
-      height,
-      age,
-      weight_desired,
-      weight_current,
-    );
-    const allNotHealthy = await Product.find({
-      'groupBloodNotAllowed[blood]': false,
-    });
-    const result = new Set();
-    for (item of Array.from({ length: allNotHealthy.length }, (_, i) => i)) {
-      result.add(
-        allNotHealthy[Math.floor(Math.random() * allNotHealthy.length)],
-      );
-      if (result.size === 4) {
-        break;
-      }
-    }
+const findNotHealthyFood = require('../../helpers/findNotHealthyFood');
+const calculator = async (req, res) => {
+  const { blood, height, age, weight_current, weight_desired } = req.body;
+  const dailyRate = calcDailyRate(height, age, weight_desired, weight_current);
 
-    res.json({
-      status: 'success',
-      code: 200,
-      data: {
-        dailyRate,
-        notHealthy: [...result],
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
+  const notHealthy = await findNotHealthyFood(blood);
+
+  res.json({
+    status: 'success',
+    code: 200,
+    data: {
+      dailyRate,
+      notHealthy,
+    },
+  });
 };
+
 module.exports = calculator;
--- routes/api/public.js
+++ routes/api/public.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { schemaValidation } = require('../../middlewares');
+const { schemaValidation, ctrlWrapper } = require('../../middlewares');
 const { public: ctrl } = require('../../controllers');
 const { joiSchema } = require('../../models/user');
 router.post(
   '/calculator',
   schemaValidation(joiSchema.userUpdate, 'Incorrect filling'),
-  ctrl.calculator,
+  ctrlWrapper(ctrl.calculator),
 );
 
 module.exports = router;
