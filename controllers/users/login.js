332577dbb5a81c1031694d5a116eefb5ecbfc487
omykhalska <omykhalska@gmail.com>
2022-07-09 23:13:39 +0300

    users controllers refactor

--- controllers/users/login.js
+++ controllers/users/login.js
@@ -10,34 +10,30 @@ require('dotenv').config();
 
 const { TOKEN_KEY } = process.env;
 
-const login = async (req, res, next) => {
-  try {
-    const { email, password } = req.body;
+const login = async (req, res) => {
+  const { email, password } = req.body;
 
-    const user = await User.findOne({ email });
+  const user = await User.findOne({ email });
 
-    if (user && (await bcrypt.compare(password, user.password))) {
-      const payload = { id: user._id, email };
-      const token = jwt.sign(payload, TOKEN_KEY, { expiresIn: '1d' });
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const payload = { id: user._id, email };
+    const token = jwt.sign(payload, TOKEN_KEY, { expiresIn: '1d' });
 
-      await User.findByIdAndUpdate(user._id, { token });
+    await User.findByIdAndUpdate(user._id, { token });
 
-      return res.status(200).json({
-        status: 'success',
-        code: 200,
-        data: {
-          token,
-          user: {
-            name: user.name,
-            email: user.email,
-          },
+    return res.status(200).json({
+      status: 'success',
+      code: 200,
+      data: {
+        token,
+        user: {
+          name: user.name,
+          email: user.email,
         },
-      });
-    }
-    throw createError(401, 'Invalid credentials');
-  } catch (error) {
-    next(error);
+      },
+    });
   }
+  throw createError(401, 'Invalid credentials');
 };
 
 module.exports = login;
--- controllers/users/logout.js
+++ controllers/users/logout.js
@@ -1,13 +1,9 @@
 const { User } = require('../../models');
 
-const logout = async (req, res, next) => {
-  try {
-    const { _id } = req.user;
-    await User.findByIdAndUpdate(_id, { token: null });
-    res.status(204).json();
-  } catch (error) {
-    next(error);
-  }
+const logout = async (req, res) => {
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { token: null });
+  res.status(204).json();
 };
 
 module.exports = logout;
