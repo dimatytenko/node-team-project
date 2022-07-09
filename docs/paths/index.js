0d5ef5374313b980eb0ca192a86cd685b2f38eb9
omykhalska <omykhalska@gmail.com>
2022-07-10 02:09:19 +0300

    Update Swagger docs

--- docs/paths/index.js
+++ docs/paths/index.js
@@ -5,12 +5,14 @@ const getAllProducts = require('./getAll-products');
 const addDiaryEntry = require('./addDiaryEntry');
 const removeDiaryEntry = require('./removeDiaryEntry');
 const countCaloriesPublic = require('./countCalories-public');
+const countCaloriesPrivate = require('./countCalories-private');
 
 module.exports = {
   paths: {
     '/users/register': { ...registerUser },
     '/users/login': { ...loginUser },
     '/users/logout': { ...logoutUser },
+    '/users/calculator': { ...countCaloriesPrivate },
     '/products': { ...getAllProducts },
     '/days': { ...addDiaryEntry },
     '/days/:dayId': { ...removeDiaryEntry },
