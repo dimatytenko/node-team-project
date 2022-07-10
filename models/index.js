22acfe5359c4e308c5d6676205d3200c25f39b96
Serhii Panasiuk <regidmail.sergej@gmail.com>
2022-07-10 22:20:53 +0300

    fix add and remov product for day

--- models/index.js
+++ models/index.js
@@ -3,6 +3,7 @@ const varDay = require('./day');
 const Day = varDay.Day;
 const User = varUser.User;
 const Product = require('./product');
+const Diary = require('./diary');
 const joiSchema = {
   userAdd: varUser.joiSchema.userAdd,
   userLogin: varUser.joiSchema.userLogin,
@@ -14,5 +15,6 @@ module.exports = {
   Day,
   User,
   Product,
+  Diary,
   joiSchema,
 };
